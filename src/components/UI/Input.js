c7bde65b4b670bc0bc5189114a3566deb0a6ba7d
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2021-12-26 14:15:17 +0200

    Add/Remove items from Card. Make the needed calculations

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import classes from './Input.module.css';
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}> {props.label}</label>
             {/* all key value pairs are added as props to input. 
                 if we have {type: 'text'}, then <input type="text" />
             */}
-            <input {...props.input} />
+            <input ref={ref} {...props.input} />
         </div>
     )
-};
+});
 
 export default Input;
